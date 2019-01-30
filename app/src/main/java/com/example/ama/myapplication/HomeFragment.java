c84ae9039333ce81996149150c9ee9909f3e67eb
tuanama13 <tuanama7@gmail.com>
2019-01-30 10:49:27 +0700

    edit home fragment

--- app/src/main/java/com/example/ama/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/ama/myapplication/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,7 @@ import butterknife.ButterKnife;
 
 public class HomeFragment extends Fragment {
 
-    @BindView(R.id.btnOrder) Button _btnOrder;
+    @BindView(R.id.btn_pesan) CardView _btnOrder;
 //    @BindView(R.id.button2) Button button2;
 
     @Nullable
