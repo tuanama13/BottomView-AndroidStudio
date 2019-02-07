7d2d848744bcafb0014f1263d74d6efc178698e9
tuanama13 <tuanama7@gmail.com>
2019-02-07 17:50:56 +0700

    edit order layout

--- app/src/main/java/com/example/ama/myapplication/Order_1.java
+++ app/src/main/java/com/example/ama/myapplication/Order_1.java
@@ -3,6 +3,7 @@ package com.example.ama.myapplication;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.zip.Inflater;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,10 +36,14 @@ public class Order_1 extends Activity {
         ButterKnife.bind(this);
         _intput_tgl.setFocusable(false);
 
-        SimpleDateFormat df_hari = new SimpleDateFormat("EEEE");
-        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+//        SimpleDateFormat df_hari = new SimpleDateFormat("EEEE");
+        String format_hari = "EE";
+        SimpleDateFormat sdf = new SimpleDateFormat(format_hari);
+
+//        String hari_ = harinya(sdf.format(c.getTime()));
+        SimpleDateFormat df = new SimpleDateFormat("dd MM yyyy");
         Date date = new Date();
-        _intput_tgl.setText(df_hari.format(date.getTime()) +", "+df.format(date));
+        _intput_tgl.setText(harinya(sdf.format(date))+", "+df.format(date));
 
 //        _btntgl = (Button)findViewById(R.id.simpan_button);
 //        _intput_tgl = (TextView)findViewById(R.id.input_tgl);
@@ -45,31 +52,47 @@ public class Order_1 extends Activity {
             @Override
             public void onClick(View view) {
 
-                _intput_tgl.setText("");
+//                _intput_tgl.setText("");
                 c = Calendar.getInstance();
+                c.getTime();
 //                int hari = c.get(Calendar.DAY_OF_WEEK);
-                int day = c.get(Calendar.DAY_OF_MONTH);
-                int month = c.get(Calendar.MONTH);
-                int year = c.get(Calendar.YEAR);
+//                int day = c.get(Calendar.DAY_OF_MONTH);
+//                int month = c.get(Calendar.MONTH);
+//                int year = c.get(Calendar.YEAR);
+//                int hari = 0;
 
 
                 date_pic = new DatePickerDialog(Order_1.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int mYear, int mMonth, int mDay) {
-                      String hari = null;
-//                         hari = c.get(Calendar.DAY_OF_WEEK);
-                         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE");
-                         hari = simpleDateFormat.format(c.get(Calendar.DAY_OF_WEEK));
+                        int hari = 0;
+                        c.set(Calendar.YEAR, mYear);
+                        c.set(Calendar.MONTH, mMonth);
+                        c.set(Calendar.DAY_OF_MONTH, mDay);
+//                        c.set(Calendar.DAY_OF_WEEK, hari);
+//                        Date d = new Date();
+//                        int day = 0;
+                        String format = "dd MM yyy";
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+
+                        String format_hari = "EE";
+                        SimpleDateFormat sdf = new SimpleDateFormat(format_hari);
+
+                        String hari_ = harinya(sdf.format(c.getTime()));
+//                        Log.v("metode",hari_);
+//                         hari = simpleDateFormat.format(c.get(Calendar.DAY_OF_WEEK));
 //
-                        _intput_tgl.setText(hari+", " +mDay + "/" +(mMonth + 1) + "/"+ mYear);
+//                        _intput_tgl.setText(simpleDateFormat.format(c.getTime()));
 
-//                        _intput_tgl.setText();
+                        _intput_tgl.setText( new StringBuilder().append(hari_).append(", ").append(simpleDateFormat.format(c.getTime())));
                     }
-                }, day, month, year);
-//                date_pic.updateDate(day, month, year);
-//                date_pic.getDatePicker().setMinDate(System.currentTimeMillis());
+                }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
 
+//                date_pic.updateDate(c.get(Calendar.DAY_OF_MONTH), c.get(Calendar.MONTH), c.get(Calendar.YEAR));
                 date_pic.show();
+                date_pic.getDatePicker().setMinDate(System.currentTimeMillis());
+
+
             }
         });
 
@@ -81,45 +104,33 @@ public class Order_1 extends Activity {
         });
     }
 
-    public static String harinya(String hari){
+    private String harinya(String hari){
             String _harinya = null;
 
-            if (hari.equals("Sunday")){
-//            _harinya = new String("Minggu");
-                _harinya =  "Minggu";
-//                System.out.println(_harinya);
-            }
-            if(hari.equals("Monday")){
-                _harinya =  "Senin";
-//                System.out.println(_harinya);
-//            _harinya = new String("Senin");
-            }
-            if(hari.equals("Tuesday")){
-//            _harinya = new String("Selasa");
-                _harinya =  "Selasa";
-//                System.out.println(_harinya);
-            }
-            if(hari.equals("Wednesday")){
-//            _harinya = new String("Rabu");
-                _harinya =  "Rabu";
-//                System.out.println(_harinya);
-            }
-            if(hari.equals("Thursday")){
-//            _harinya = new String("Kamis");
-                _harinya =  "Kamis";
-//                System.out.println(_harinya);
-            }
-            if(hari.equals("Friday")){
-//            _harinya = new String("Jumat");
-                _harinya =  "Jumat";
-//                System.out.println(_harinya);
-            }
-            if(hari.equals("Saturday")) {
-//            _harinya = new String("Sabtu");
-                _harinya = "Sabtu";
-//                System.out.println(_harinya);
+            switch (hari){
+                case "Sun":
+                    _harinya = "Min";
+                    break;
+                case "Mon":
+                    _harinya = "Sen";
+                    break;
+                case "Tue":
+                    _harinya = "Sel";
+                    break;
+                case "Wed":
+                    _harinya = "Rab";
+                    break;
+                case "Thu":
+                    _harinya = "Kam";
+                    break;
+                case "Fri":
+                    _harinya = "Jum";
+                    break;
+                case "Sat":
+                    _harinya = "Sab";
+                    break;
             }
 
-           return new String(_harinya);
+           return _harinya ;
     }
 }
